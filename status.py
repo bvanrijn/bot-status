252cb6df1ddf242ff7ff57dd35bde0d4d5ce0ab7
bvanrijn <bvanrijn@noreply.users.github.com>
2016-06-10 18:08:31 +0200

    small changes

--- status.py
+++ status.py
@@ -3,7 +3,7 @@ import time
 import requests
 import json
 
-# TODO: change this to your own token
+# TODO: change this to your own status bot token
 bot = telegram.Bot(token='123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
 # TODO: change this name to something unique
 dweet_name = "telegram-bot-ping"
