757136f4caf4bb043310f22776aec4c740f5111d
bvanrijn <bvanrijn@users.noreply.github.com>
2016-06-10 15:14:45 +0200

    bot template is working
    bot-status requires Python 3.5 now

--- bot-template.py
+++ bot-template.py
@@ -1,11 +1,17 @@
 import telegram
 import time
 import requests
+import asyncio
 
 bot = telegram.Bot(token='123456789:YourBotToken')
 dweet_name = "telegram-bot-ping"
 dweet_base = "https://dweet.io/dweet/for/" + dweet_name + "?"
 
-while True:
-    request = requests.get(dweet_base + "time=" + str(int(time.time())))
-    time.sleep(30)
+async def ping():
+    while True:
+        request = requests.get(dweet_base + "time=" + str(int(time.time())))
+        time.sleep(30)
+
+loop = asyncio.get_event_loop()  
+loop.run_until_complete(ping())  
+loop.close()
