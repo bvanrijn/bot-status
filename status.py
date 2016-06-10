bf7962f70d2ec01a572d16ac5ea35fd5973cecf1
bvanrijn <bvanrijn@users.noreply.github.com>
2016-06-10 15:30:21 +0200

    Update status.py

--- status.py
+++ status.py
@@ -5,20 +5,12 @@ import json
 
 # TODO: change this to your own token
 bot = telegram.Bot(token='123456789:StatusBotToken')
+# TODO: change this name to something unique
 dweet_name = "telegram-bot-ping"
 dweet = "https://dweet.io/get/latest/dweet/for/" + dweet_name
 
 while True:
-    '''
-    try:
-        ping = requests.get(dweet).contents
-        print ping
-    except Exception:
-        continue
-    '''
-
     ping = requests.get(dweet).content
-
     ping = json.loads(ping)
 
     if ping["this"] == "succeeded":
@@ -29,6 +21,7 @@ while True:
             chat_id = bot.getUpdates()[-1].message.chat_id
             bot.sendMessage(chat_id=chat_id, text="More than 5 minutes since last ping")
         # TODO: Add more messages.... 1 hour, 12 hours, etc.
+        # TODO: Add proper exception handling
         else:
             continue
 
