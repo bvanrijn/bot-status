158c0cee852ced7c1588870faf32520d8c810814
bvanrijn <bvanrijn@noreply.users.github.com>
2016-06-11 16:45:07 +0200

    Fixed some things, added some things.

--- status.py
+++ status.py
@@ -11,29 +11,31 @@ bot = telegram.Bot(token='123456:STA-TUS1234ghIkl-zyx57W2v1u123ew11')
 # TODO: Change this name to something unique
 dweet_name = "telegram-bot-ping"
 dweet = "https://dweet.io/get/latest/dweet/for/" + dweet_name
+sentAlert = False
 
 while True:
-    try:
-        ping = requests.get(dweet).content
-        ping = json.loads(ping)
-    except:
-        print("Something went wrong. Trying again.")
-        continue
+    ping = requests.get(dweet).content
+    ping = json.loads(ping)
 
     # Yay! Success! We shall continue
     if ping["this"] == "succeeded":
         # Get the last ping time
         last_ping = ping["with"][0]["content"]["time"]
         # 5 minutes without a ping? Let's send an alert.
-        if time.time() - last_ping > 5 * 60:
+        if sentAlert == False and time.time() - last_ping > 5 * 60:
             print("More than 5 minutes since last ping. Please check on your bot.")
             chat_id = bot.getUpdates()[-1].message.chat_id
             bot.sendMessage(chat_id=chat_id, text="More than 10 minutes since last ping.\nPlease check on your bot.")
-        # FIXME: Prevent from sending too many messages. One every hour should be enough
+            sentAlert = True
+        # FIXME: Send one alert every hour if the bot is broken
+        # FIXME: Fix having to restart the status script if the bot comes up again
         # TODO:  Add more exception handling
         # TODO:  Add more messages.... 1 hour, 12 hours, etc.
         else:
-            print("All is probably well")
+            if sentAlert == True:
+                print("Already sent an alert")
+            else:
+                print("All is probably well")
 
     # Sleep for 2 minutes and 30 seconds
     time.sleep(2.5 * 60)
